c19255e22940dc3b38e78a03ca973378261e8f2d
YOUR NAME <YOUR@EMAIL.com>
2018-04-04 01:49:55 +0300

    add index.rb

---
+++ index.rb
@@ -0,0 +1,43 @@
+# My first comment
+# I like to do comment
+
+  a = 5       #Inetger
+
+  b = 1.6     #Float
+
+  c = 'I love Ruby'   #String
+
+  e = 2.0     # Inetger
+
+  d = 4       #Inetger
+
+=begin
+        first_name = 'John'
+
+puts full_name
+
+=end
+
+total1 = a + \
+            b
+total2 = a / e
+
+total3 = d**2
+
+puts total1
+
+puts total2
+
+puts total3
+
+    first_name = 'John'
+    last_name = 'Doe'
+    age  = 20
+
+full_name = first_name + last_name
+
+full_name2 = "#{first_name} #{last_name} | Age: #{age}"
+
+# puts full_name
+
+puts full_name2
