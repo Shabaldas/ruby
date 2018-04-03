429879586be12db47ac92b929bf5e84c8213a681
YOUR NAME <YOUR@EMAIL.com>
2018-04-04 01:58:35 +0300

    add index2

---
+++ index2.rb
@@ -0,0 +1,19 @@
+=begin
+  c = [2, 4, 6]
+  x = 1
+  y = 3
+
+  d = [x + y, x - y, x + y**2]
+  puts d
+
+  b = %w[ruby is cool]
+  puts b
+
+  e = %W[ruby is cool]
+  puts e
+
+=end
+
+  x = ['a', 'b', 'c', 'd', 'e']
+
+  puts x[0..1], x[0,2], x[-1,3], x[-2,1]
